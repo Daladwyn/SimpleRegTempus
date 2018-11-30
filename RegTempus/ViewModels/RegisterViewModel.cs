78c17f904c74ca4ffd199047a36b4052c9f840c4
Christian Levin <christian.johansson@gmail.com>
2018-11-30 09:16:12 +0100

    minor enhancements to login/register view

--- RegTempus/ViewModels/RegisterViewModel.cs
+++ RegTempus/ViewModels/RegisterViewModel.cs
@@ -8,18 +8,21 @@ namespace RegTempus.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage ="You must enter a valid email adress")]
         [Display(Name = "User name")]
         [MaxLength(100)]
         public string UserName { get; set; }
 
         [Required]
-        [DataType(DataType.Password)]
+        [Display(Name ="Password(min 6 char, one digit, one symbol, one uppercase and one lowercase letter)")]
+        [DataType(DataType.Password, ErrorMessage = "Password must contain one digit, one symbol, one lowercase and one uppercase letter")]
         public string Password { get; set; }
 
+        [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
 
+        [Required]
         [MaxLength(50)]
         public string LastName { get; set; }
     }
