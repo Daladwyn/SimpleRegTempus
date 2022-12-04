2be23835e97c3bed70f7670e9b4fb0c64f4701bb
Christian Levin <christian.johansson@gmail.com>
2022-12-04 23:48:40 +0100

    Support nugets are added for Entity frameworkCore and Identity.

--- RegTempus/Controllers/HomeController.cs
+++ RegTempus/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RegTempus.Models;
 using RegTempus.Services;
--- RegTempus/Startup.cs
+++ RegTempus/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RegTempus.Models;
